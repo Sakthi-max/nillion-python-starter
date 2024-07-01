aea74ba438c1824f2e3614283a01c8bb13e8470d
Sakthi-max <iamsakthi.2004@gmail.com>
2024-07-01 22:22:04 +0530

    Update my_first_program.py

--- programs/my_first_program.py
+++ programs/my_first_program.py
@@ -1,12 +1,19 @@
 from nada_dsl import *
 
 def nada_main():
+    # Define parties
     party1 = Party(name="Party1")
-    my_int1 = SecretInteger(Input(name="my_int1", party=party1))
-    my_int2 = SecretInteger(Input(name="my_int2", party=party1))
 
-    # Perform some computation using my_int1 and my_int2
-    result = my_int1 + my_int2  # Example: Sum of two secret integers
+    # Define inputs (two SecretInteger inputs)
+    input1 = SecretInteger(Input(name="input1", party=party1))
+    input2 = SecretInteger(Input(name="input2", party=party1))
 
-    # Define the output to be returned
-    return [Output(result, "sum_result", party1)]
+    # Compute the squares of inputs
+    square1 = input1 * input1
+    square2 = input2 * input2
+
+    # Compute the sum of squares
+    sum_of_squares = square1 + square2
+
+    # Output the sum of squares result
+    return [Output(sum_of_squares, "sum_of_squares_output", party1)]
